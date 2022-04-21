135869047529726ef84ef57e6ae9da3060e5d261
Любовь <103143988+lemasalova@users.noreply.github.com>
2022-04-21 12:21:29 +0300

    Ходит по разным сайтам

--- GoogleBot.js
+++ GoogleBot.js
@@ -11,14 +11,27 @@
 // ==/UserScript==
 
 
-let keywords = ["Редакции - это резервные копии","10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress"];
+let sites = {"napli.ru":["Редакции — это резервные копии",
+                         "10 самых популярных шрифтов от Google",
+                         "Отключение редакций и ревизий в WordPress",
+                         "Вывод произвольных типов записей и полей в WordPress",],
+             "auto.ru":["продажа автомобилей", "каталог автомобилей", "форумы на автору",]};
+let site = Object.keys(sites)[getRandom(0, Object.keys(sites).lenght)];
+let keywords = sites[site];
 let keyword = keywords[getRandom(0, keywords.length)];
 let btnK = document.getElementsByName("btnK")[0];
 let links = document.links;
 let googleInput = document.getElementsByName("q")[0];
 
 if(btnK !== undefined) {
-  //console.log(keyword);
+  document.cookie = "site=" + site;
+} else if (location.hostname == "www.google.com") {
+  site = getCookie("site");
+} else {
+  site = location.hostname;
+}
+if(btnK !== undefined) {
+  document.cookie = "site=" + site;
   let i = 0;
   let timerId = setInterval(() => {
     googleInput.value += keyword[i];
@@ -28,28 +41,28 @@ if(btnK !== undefined) {
       btnK.click();
     }
   },500);
-}else if (location.hostname == "napli.ru") {
+}else if (location.hostname == site) {
 
   setInterval(()=>{
     let index = getRandom(0, links.length);
     if (getRandom (0, 101) >= 80) {
       location.href = "https://www.google.com/";
-    } else if(links[index].href.indexOf("napli.ru") !== -1) {
+    } else if(links[index].href.indexOf(site) !== -1) {
       links[index].click();
     }
-  }, getRandom(1500, 5000));
+  }, getRandom(3000, 5000));
   //console.log(Мы на "napli.ru");
 
 }else {
   let nextGooglePage = true;
   for (let i = 0; i < links.length; i++) {
-    if (links[i].href.indexOf("napli.ru") !== -1) {
+    if (links[i].href.indexOf(site) !== -1) {
       let link = links[i]
       nextGooglePage = false;
       // console.log("Нашел строку " + links[i]);
       setTimeout(()=>{
         link.click();
-      }, getRandom(1500,4000));
+      }, getRandom(2500,4000));
       break;
     }
   }
@@ -65,4 +78,11 @@ if(btnK !== undefined) {
 
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
